b0e0cb1d6272f40fdbe1514c5f1edc223ea5f84b
mtanveer78 <tanveerhabtani308@gmail.com>
2023-03-14 23:32:31 +0500

    first commit

---
+++ src/context/Alert/Alert.js
@@ -0,0 +1,25 @@
+// import AlertContext from "./alertContext";
+// import React from 'react'
+
+// const Alert = (props) => {
+//     const [alerts, setAlerts] = useState({ message: null, type: null });
+
+    // const setmsg = (message, type)=>{
+    //     setAlerts({
+    //         msg: message,
+    //         type: type
+    //     })
+    //     setTimeout(() => {
+    //         setAlerts(null);
+    //     }, 1500);
+    // }
+
+    
+//     return (
+//         <AlertContext.Provider value={{setmsg, alerts}}>
+//             {props.children}
+//         </AlertContext.Provider>
+//     )
+// }
+
+// export default Alert
